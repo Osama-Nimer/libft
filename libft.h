86d5b29851a7f57b325fc09d4cdbaa1901285e06
Osama Nimer <osamafares23@yahoo.com>
2025-08-26 23:44:35 +0300

    libft.h norminenatted.

--- libft.h
+++ libft.h
@@ -13,27 +13,30 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
-# include <stdlib.h>
-#include <stddef.h>
+# include <stddef.h>
 
-int	ft_atoi(const char *str);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isprint(char c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isprint(char c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
 void	ft_bzero(void *s, size_t n);
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize); 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strdup(const char *s1); 
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t len);
+
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+char	*ft_strdup(const char *s1);
 
 #endif
